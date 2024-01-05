6d3217030e1bb1eb914fadfb90fe30a645e5ee89
Kamal <malikleet1337@gmail.com>
2024-01-05 13:03:06 +0100

    fixed

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+
 from sys import argv
 
 if __name__ == "__main__":
-    # Extract command-line arguments starting from index 1
-    arguments = argv[1:]
 
-    # Convert arguments to integers and sum them
+    arguments = argv[1:]
+    
     result = sum(int(arg) for arg in arguments)
 
     print(result)
-    
