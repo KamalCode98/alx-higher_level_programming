ff323cfce0a7ac48ff1fbf4836f0b700350450f4
Kamal <malikleet1337@gmail.com>
2024-01-05 13:48:06 +0100

    test6

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    from sys import argv
 
-if __name__ != "__main__":
-    exit()
+    argv_len = len(argv)
+    result = 0
 
-argc = len(sys.argv) - 1
+    for i in range(1, argv_len):
+        result += int(argv[i])
 
-i = 0
-result = 0
-for arg in sys.argv:
-    if i != 0:
-        result += int(arg)
-    else:
-        i += 1
-print("{:d}".format(result))
+    print("{:d}".format(result))
