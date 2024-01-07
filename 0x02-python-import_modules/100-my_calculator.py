bf11463b4867e56ff5b84f9df6bbbc1514d4c767
Kamal <malikleet1337@gmail.com>
2024-01-07 19:02:33 +0100

    commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -17,8 +17,9 @@ elif argv[2] == '*':
 elif argv[2] == '/':
     func = div
 else:
-    print("Unknown operator: Available operators: +, -, *, and /")
+    print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
+    
 result = func(int(argv[1]), int(argv[3]))
 print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
                                      argv[2], int(argv[3]), result))
