55ce271d7724d25d8b629480a236797019f6897c
Kamal <malikleet1337@gmail.com>
2024-01-23 22:17:33 +0100

    pycodestyl

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,6 +10,16 @@ Class Square that defines a square
 
 class Square:
     def __init__(self, size=0) -> None:
+        """
+        Constructor function
+
+        Args:
+        param1 (self): instance
+        param2 (int): size of the square
+
+        Returns:
+        - None
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
