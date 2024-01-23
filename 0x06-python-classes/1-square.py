6834b3f051fabb9557307ec36d31571b553aba17
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-01-23 11:29:03 -0800

    fixing pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 """
-class Square that defines a square by: (based on 0-square.py) 
+class Square that defines a square by: (based on 0-square.py)
 """
+
 
 class Square:
     """square with size"""
-
     def __init__(self, size) -> None:
         self.__size = size
