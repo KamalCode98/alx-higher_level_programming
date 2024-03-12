95b546240de3063c20367c660ce5183abe564bf6
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-03-12 08:33:40 -0700

    First add

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- Inserts a new row into first_table with id=89 and name='Best School'
+-- execution 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+
