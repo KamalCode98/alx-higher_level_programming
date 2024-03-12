c31e540818b4f7482301c3efb938f806d626d856
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-03-12 08:10:54 -0700

    Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Script that prints the ful description of the table first_table from the database hbtn_0c_0
+-- Execution 5-full_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SHOW CREATE TABLE first_table;
