3eaa449b245d6d98f464b4aeb4cdbf5a5384786a
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-03-12 09:08:12 -0700

    List by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Selects score and name from second_table ordered by score in descending order
+-- Execution 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SELECT score, name FROM second_table ORDER BY score DESC;
