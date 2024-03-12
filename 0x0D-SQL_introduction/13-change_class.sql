0a4d31ba885c3c087f0462401080d58e3584b2be
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-03-12 16:23:14 -0700

    fix

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,3 @@
 -- Deletes rows from second_table where the score is less than or equal to 5
--- Execute cat 13-change_class.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+DELETE FROM second_table WHERE score <= 5;
 
-DELETE FROM seconde_table WHERE score <= 5;
