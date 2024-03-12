c5633d5ec65f4213a3eaac3fd9aefd942858eb4f
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-03-12 16:19:28 -0700

    say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- Selects score and name from second_table where name is not null, ordered by score in descending order
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+
