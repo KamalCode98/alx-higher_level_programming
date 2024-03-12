682d64055856e5dc1e2ba856c190591ba62b0c06
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-03-12 16:04:01 -0700

    average

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Calculates the average score of all records in second_table
+-- Execution 14-average.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+
+SELECT AVG(score) AS average FROM second_table;
