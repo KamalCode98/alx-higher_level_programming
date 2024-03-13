0c5120b995363536fc5a9ab8e68031fd6da2a362
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-03-13 05:31:42 -0700

    Task 5 : Unique ID

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Script that creates the table unique_id
+-- Execution cat 5-unique_id.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
