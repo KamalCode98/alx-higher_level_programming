5f7527d5f4094e450a4eb1910f530af46771a22c
Kamal <malikleet1337@gmail.com>
2024-04-08 23:01:11 +0000

    semistandart fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+
+const c  = 'C is fun\n';
+const python = 'Python is cool\n';
+const js = 'JavaScript is amazing\n';
+
+console.log(c + python + js);
--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
+
+const args = process.argv;
+let text;
+
 if (process.argv.length === 2) {
   console.log('No argument');
 } else if (process.argv.length === 3) {
@@ -6,3 +10,5 @@ if (process.argv.length === 2) {
 } else {
   console.log('Arguments found');
 }
+
+console.log('text');
