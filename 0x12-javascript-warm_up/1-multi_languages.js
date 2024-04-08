8b64df6d03d213af690f46b330dbb4f5582c1c1e
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-04-08 16:19:21 -0700

    check4 test

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
 
-const c = 'C is fun\n';
-const python = 'Python is cool\n';
-const js = 'JavaScript is amazing\n';
-
-console.log(c + python + js);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScripit is amazing');
