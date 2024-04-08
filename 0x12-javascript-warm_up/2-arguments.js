d7f5bec544c084cfe5d9d16959b2e7695182a240
Kamal Abdelmalek <malikleet1337@gmail.com>
2024-04-08 16:22:35 -0700

    check 5 is driving me crzay

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv;
-let message;
-
-if (args.length <= 2) {
-  message = 'No argument';
-} else if (args.length === 3) {
-  message = 'Argument found';
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-  message = 'Arguments found';
+  console.log('Arguments found');
 }
-
-console.log(message);
