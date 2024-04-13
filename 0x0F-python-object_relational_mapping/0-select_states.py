378533402aeaa9c763e58d57901bf2fbcc26a3a1
Kamal <malikleet1337@gmail.com>
2024-04-13 12:38:39 +0000

    fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,39 +3,18 @@
 import MySQLdb
 from sys import argv
 
-def connect_to_database(host, port, user, password, database):
-    """Connects to a MySQL database and returns the connection object."""
-    return MySQLdb.connect(host=host, port=port, user=user, passwd=password, db=database)
-
-def execute_query(connection, query):
-    """Executes a SQL query on the given database connection and returns the result."""
-    cursor = connection.cursor()
-    cursor.execute(query)
-    result = cursor.fetchall()
-    cursor.close()
-    return result
-
-def print_results(results):
-    """Prints the results of a query."""
-    for row in results:
-        print(row)
-
 if __name__ == '__main__':
-    if len(argv) != 4:
-        print("Usage: {} username password database".format(argv[0]))
-        exit(1)
 
-    host = "localhost"
-    port = 3306
-    user = argv[1]
-    password = argv[2]
-    database = argv[3]
+    # make a connection to the database
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-    try:
-        db = connect_to_database(host, port, user, password, database)
-        results = execute_query(db, "SELECT * FROM states")
-        print_results(results)
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error [{}]: {}".format(e.args[0], e.args[1]))
-        exit(1)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+
+    rows = cur.fetchall()
+    for i in rows:
+        print(i)
+
+    cur.close()
+    db.close()
